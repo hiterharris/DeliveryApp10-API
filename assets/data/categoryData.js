96ac7a7a8b75bf2049bd72192358581c182b7a80
Hiter Harris <hiterharris@comcast.net>
2021-03-12 10:22:00 -0500

    update categoryData

--- assets/data/categoryData.js
+++ assets/data/categoryData.js
@@ -1,23 +1,26 @@
-import { icons } from '../../constants';
-
 export const categoryData = [
     {
         id: 1,
+        name: "All",
+        icon: 'https://bit.ly/3qHjARO',
+    },
+    {
+        id: 2,
         name: "Vapes",
         icon: 'https://bit.ly/3cu5Jtn',
     },
     {
-        id: 2,
+        id: 3,
         name: "Beer",
         icon: 'https://bit.ly/3li4CRu',
     },
     {
-        id: 3,
+        id: 4,
         name: "Wine",
         icon: 'https://bit.ly/30EZVrc',
     },
     {
-        id: 4,
+        id: 5,
         name: "Tobacco",
         icon: 'https://bit.ly/3qH1pMm',
     }
