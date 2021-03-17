76e0af3c687e33e67e7d64db2aafe58ebf5d0393
hiterharris <hiterharris@comcast.net>
2021-03-17 10:53:35 -0400

    new data

--- api/products/products-router.js
+++ api/products/products-router.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const products = require('./productsData');
+const newProductsData = require('./newProductsData');
 
 router.get('/', (req, res) => {
   res.send(products)
+});
+
+router.get('/new', (req, res) => {
+  res.send(newProductsData)
 });
 
 module.exports = router;
