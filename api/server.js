997ce1522af6c90a252d8ea18fe35d7f5c95476d
Hiter Harris <hiterharris@comcast.net>
2021-05-19 16:01:07 -0400

    setup authentication

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('../auth/auth-router.js');
+const restricted = require('../auth/restricted-middleware');
+const usersRouter = require('../users/users-router.js');
 const productsRouter = require('./products/products-router');
+
 
 const server = express();
 
@@ -10,6 +14,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/auth', authRouter);
+server.use('/users', restricted, usersRouter);
 server.use('/products', productsRouter);
 server.get('/', (req, res) => {
     res.send('Food Delivery App API')
